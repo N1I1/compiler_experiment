c99a117f59489b58077f4fa03049820ac2aba67b
N1I1 <he231920@gmail.com>
2024-04-26 22:51:26 +0800

    test

---
+++ 3/lr1.py
@@ -0,0 +1,14 @@
+from ..e2.grammar import Grammar
+class LR1Parser:
+    def __init__(self, grammar: Grammar):
+        self.grammar = grammar
+    
+    def compute_lr1_sets(self):
+        raise NotImplemented
+    
+    def build_lr1_parsing_table(self):
+        self.tawwww
+        raise NotImplemented
+    
+    def parse(self, input):
+        raise NotImplemented
